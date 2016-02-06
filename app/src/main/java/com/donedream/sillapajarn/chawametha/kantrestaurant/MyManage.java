20d23d5a2e83d42d451c37e060458768f8a01ed3
kan555 <kan-sign-in@Hotmail.com>
2016-02-06 13:30:51 +0700

    delete all data

--- app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/MyManage.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/MyManage.java
@@ -1,5 +1,6 @@
 package com.donedream.sillapajarn.chawametha.kantrestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,16 @@ public class MyManage {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase wriSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String user_TABLE = "userTABLE";
+    public static final String food_TABLE = "foodTABLE";
+    public static final String colum_id = "_id";
+    public static final String colum_user = "User";
+    public static final String colum_pass = "Password";
+    public static final String colum_name = "Name";
+    public static final String colum_food = "Food";
+    public static final String colum_price = "Price";
+    public static final String colum_source = "Source";
+
     public MyManage(Context context) {
 
         //Create and Connect Database
@@ -20,5 +31,33 @@ public class MyManage {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }   // Constructor
+
+    public long addNewValue(int intTable,
+                            String strColumn2,
+                            String strColumn3,
+                            String strColumn4) {
+
+        ContentValues objContentValues = new ContentValues();
+        long longReturn = 0;
+
+        switch (intTable) {
+            case 0:
+
+                objContentValues.put(colum_user, strColumn2);
+                objContentValues.put(colum_pass, strColumn3);
+                objContentValues.put(colum_name, strColumn4);
+                wriSqLiteDatabase.insert(user_TABLE, null, objContentValues);
+
+                break;
+            case 1:
+                objContentValues.put(colum_food, strColumn2);
+                objContentValues.put(colum_price, strColumn3);
+                objContentValues.put(colum_source, strColumn4);
+                wriSqLiteDatabase.insert(food_TABLE, null, objContentValues);
+                break;
+        }   //switch
+
+        return longReturn;
+    }
 
 }   // Main Class
