77b532a9786cdf3201ffb53e45c3751fb14689ca
kan555 <kan-sign-in@Hotmail.com>
2016-02-06 15:03:14 +0700

    begin JSON to SQLite

--- app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/MainActivity.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/MainActivity.java
@@ -1,8 +1,12 @@
 package com.donedream.sillapajarn.chawametha.kantrestaurant;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +27,43 @@ public class MainActivity extends AppCompatActivity {
         //clean data
         cleanData();
 
+        //synchronized JSON to SQLite
+        synJSONtoSQLite();
+
     }   // Main Method
+
+    private void synJSONtoSQLite() {
+
+        //change policy for and to connect to http directly
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        int intTable = 1;
+        String tag = "Restaurant"
+
+        while (intTable <= 2) {
+
+            //1. create inputStream
+            InputStream objInputStream = null;
+            String StrURLuser = "http://swiftcodingthai.com/6feb/php_get_data_kant.php";
+            String StrURLfppd = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+
+            try {
+
+            } catch (Exception e) {
+                Log.d(tag, "ImputStream ==> " + e.toString());
+            }
+
+            //2. create JSON String
+
+            //3.Update SQLite
+
+
+            intTable += 1;
+        }   //while
+
+    }   // synJSONtoSQLite
 
     private void cleanData() {
 
