9ff7e1c7fe29cd7d46e9e75939be26549d11b232
kan555 <kan-sign-in@Hotmail.com>
2016-02-07 10:06:32 +0700

    syn Json finished

--- app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/MainActivity.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/MainActivity.java
@@ -1,12 +1,23 @@
 package com.donedream.sillapajarn.chawametha.kantrestaurant;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.net.http.HttpResponseCache;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,24 +51,95 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(myPolicy);
 
         int intTable = 1;
-        String tag = "Restaurant"
+        String tag = "Restaurant";
 
         while (intTable <= 2) {
 
             //1. create inputStream
             InputStream objInputStream = null;
-            String StrURLuser = "http://swiftcodingthai.com/6feb/php_get_data_kant.php";
-            String StrURLfppd = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+            String strURLuser = "http://swiftcodingthai.com/6feb/php_get_data_kant.php";
+            String strURLfood = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+            HttpPost objHttpPost = null;
 
             try {
 
+                HttpClient objHttpClient = new DefaultHttpClient();
+                switch (intTable) {
+                    case 1:
+                        objHttpPost = new HttpPost(strURLuser);
+                        break;
+                    case 2:
+                        objHttpPost = new HttpPost(strURLfood);
+                        break;
+                    default:
+                        break;
+                }   //switch
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
             } catch (Exception e) {
-                Log.d(tag, "ImputStream ==> " + e.toString());
+                Log.d(tag, "InputStream ==> " + e.toString());
             }
 
-            //2. create JSON String
+            //2. create JSON to String
+            String strJSON = null;
+
+            try {
+
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+                StringBuilder objStringBuilder = new StringBuilder();
+                String strLine = null;
+
+                while ((strLine = objBufferedReader.readLine()) != null) {
+                    objStringBuilder.append(strLine);
+                }   //while
+
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();
+
+
+            } catch (Exception e) {
+                Log.d(tag, "strJSON ===>" + e.toString());
+            }
 
             //3.Update SQLite
+
+            try {
+
+                JSONArray objJsonArray = new JSONArray(strJSON);
+                for (int i = 0; i < objJsonArray.length(); i++) {
+
+                    JSONObject jsonObject = objJsonArray.getJSONObject(i);
+
+                    switch (intTable) {
+                        case 1:
+
+                            //get value from userTABLE
+                            String strUser = jsonObject.getString(MyManage.colum_user);
+                            String strPassword = jsonObject.getString(MyManage.colum_pass);
+                            String strName = jsonObject.getString(MyManage.colum_name);
+
+                            objMyManage.addNewValue(0, strUser, strPassword, strName);
+
+                            break;
+                        case 2:
+
+                            //get values from FoodTABLE
+                            String strFood = jsonObject.getString(MyManage.colum_food);
+                            String strPrice = jsonObject.getString(MyManage.colum_price);
+                            String strSource = jsonObject.getString(MyManage.colum_source);
+
+                            objMyManage.addNewValue(1, strFood, strPrice, strSource);
+
+                            break;
+                    }   //switch
+                }   //for
+
+            } catch (Exception e) {
+                Log.d(tag, "Update ==>" + e.toString());
+            }
 
 
             intTable += 1;
