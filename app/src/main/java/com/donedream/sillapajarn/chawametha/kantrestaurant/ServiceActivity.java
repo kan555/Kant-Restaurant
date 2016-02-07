2de79abbfef4e46d0e7f8ceba4cf12431963bb09
kan555 <kan-sign-in@Hotmail.com>
2016-02-07 12:54:28 +0700

    show view finished

--- app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/ServiceActivity.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/ServiceActivity.java
@@ -2,12 +2,43 @@ package com.donedream.sillapajarn.chawametha.kantrestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //explicit
+    private TextView shownameTextView;
+    private Spinner deskSpinner;
+    private ListView foodListView;
+    private String officerString,deskString, foodString, amountString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        //bind widget
+        bindwidget();
+
+        //show view
+        showView();
+
+    }   //main method
+
+    private void showView() {
+
+        //receive from intent
+        officerString = getIntent().getStringExtra("Name");
+        shownameTextView.setText(officerString);
+
     }
-}
+
+    private void bindwidget() {
+        shownameTextView = (TextView) findViewById(R.id.textView2);
+        deskSpinner = (Spinner) findViewById(R.id.spinner);
+        foodListView = (ListView) findViewById(R.id.listView);
+    }
+
+}   //main class
