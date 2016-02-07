f2b1a9615fede7f824435a0e2043006ad1c74b6f
kan555 <kan-sign-in@Hotmail.com>
2016-02-07 13:16:26 +0700

    desk spinner finished

--- app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/ServiceActivity.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/kantrestaurant/ServiceActivity.java
@@ -2,6 +2,9 @@ package com.donedream.sillapajarn.chawametha.kantrestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,7 +28,32 @@ public class ServiceActivity extends AppCompatActivity {
         //show view
         showView();
 
+        //show desk
+        showDesk();
+
     }   //main method
+
+    private void showDesk() {
+
+        final String[] showDeskStrings = {"Desk 1", "Desk 2", "Desk 3", "Desk 4", "Desk 5",
+                "Desk 6", "Desk 7", "Desk 8", "Desk 9", "Desk 10"};
+        ArrayAdapter<String> deskAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, showDeskStrings);
+        deskSpinner.setAdapter(deskAdapter);
+        //select can change select which
+        deskSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                deskString = showDeskStrings[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                deskString = showDeskStrings[0];
+            }
+        });
+
+    }   //show desk
 
     private void showView() {
 
